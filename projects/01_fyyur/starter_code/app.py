264134c48ca70bb07dc2d5671f72a4384943a06a
antoniofurtado <antoniordf@hotmail.com>
2022-03-10 21:08:26 +0000

    Updated Venue model with a solution for genre
    This solution comes from: https://knowledge.udacity.com/questions/63411

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
-    genre = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     comment = db.Column(db.Text(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
