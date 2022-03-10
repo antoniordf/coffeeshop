6faa5be06a3932145258ba29ba6a0c26941a7b3d
antoniofurtado <antoniordf@hotmail.com>
2022-03-10 20:10:12 +0000

    Updated venue model to include additional columns

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -33,13 +33,16 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    genre = db.Column(db.String(120))
+    comment = db.Column(db.Text(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
