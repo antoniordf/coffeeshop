4e1a52c7e351b2a18d3c9d74e1167fd03ff26227
antoniofurtado <antoniordf@hotmail.com>
2022-06-08 12:20:02 +0100

    Implemented delete_drink

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -141,6 +141,23 @@ def update_drink(drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(drink_id):
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+        if drink is None:
+            abort(404)
+        
+        drink.delete()
+
+        return jsonify({
+            'success': True,
+            'delete': drink.id
+        }), 200
+    except:
+        abort(422)
 
 
 # Error Handling
