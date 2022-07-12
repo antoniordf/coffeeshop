7b02079b3f069406dab7af9ef7f1bb4e2a4a45de
antoniofurtado <antoniordf@hotmail.com>
2022-07-13 00:14:38 +0100

    Fixed 'no atribute question' bug when testing
    create_question tests produced an error due to the above

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -16,6 +16,13 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name) #Fixed depracation error. See: https://knowledge.udacity.com/questions/562092
+        #I am adding the below dict to solve an error I was getting when running the create_question tests (AttributeError: 'TriviaTestCase' object has no attribute 'question'): https://knowledge.udacity.com/questions/247875
+        self.question = {
+            'question': 'What is the color of the sky?',
+            'answer': 'blue',
+            'category': 1,
+            'difficulty': 1
+        }
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context (whatever that means).
